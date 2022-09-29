e2a72980c607c012e5a2064d33a9f96328d764f3
LuisBarbosa02 <luiscastro250302@gmail.com>
2022-09-29 09:33:25 -0300

    Added attack, lose hp and die to Player and Monster classes.

--- code_behind.py
+++ code_behind.py
@@ -16,6 +16,21 @@ class Player:
     self.life = life
     self.attack_dict = attack_dict
     self.dead = False
+  
+  def attack(self, attack_name, enemy):
+    if attack_name not in attack_dict:
+      return logger.debug("That attack it's not available!")
+    attack_damage = self.attack_dict[attack_name][0]
+    enemy.lose_hp(attack_damage)
+
+  def lose_hp(self, attacker_damage):
+    self.life -= attacker_damage
+    if self.life <= 0:
+      self.life = 0
+      return self.die()
+  
+  def die(self):
+    self.dead = True
 
 class Monster:
   def __init__(self, type, life, attack_dict):
@@ -23,3 +38,18 @@ class Monster:
     self.life = life
     self.attack_dict = attack_dict
     self.dead = False
+  
+  def attack(self, attack_name, enemy):
+    if attack_name not in attack_dict:
+      return logger.debug("That attack it's not available!")
+    attack_damage = self.attack_dict[attack_name][0]
+    enemy.lose_hp(attack_damage)
+
+  def lose_hp(self, attacker_damage):
+    self.life -= attacker_damage
+    if self.life <= 0:
+      self.life = 0
+      return self.die()
+  
+  def die(self):
+    self.dead = True
